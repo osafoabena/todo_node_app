af44602a5fe1a090bd59559dbcbdff6a31427ffa
benedicta <addicobenedicta@gamil.com>
2021-12-15 10:09:11 +0000

    mongoose db atlas

---
+++ controllers/todoController.js
@@ -0,0 +1,25 @@
+const todo = require('../models/todo');
+
+function insertTodo(){
+
+}
+
+function updateTodoById(){
+
+
+}
+
+function deleteTodoById(){
+
+}
+
+function getTodoById(){
+
+}
+
+function getAllTodos(req, res){
+
+}
+module.exports = {
+    insertTodo, updateTodoById, deleteTodoById, getTodoById, getAllTodos
+}
--- express_app.js
+++ express_app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const todoController = require('./controllers/todoController');
 const server = express();
 const mongo_db_url = 'mongodb+srv://ms_naa:sprinkles93@cluster0.vegxf.mongodb.net/todos_db?retryWrites=true&w=majority'
 
 
 
 
-server.get('/todos', function(req, res){
-   res.status(200).json('You have been given access to all todos');
-});
-server.post('/todos', function(req, res){
-    res.status(200).json('A new todo has been created');
-});
-server.put('/todos', function(req, res){
-    res.status(200).json('Todo updated');
-});
-server.delete('/todos', function(req, res){
-    res.status(200).json('Todo has been deleted');
-});
+server.get('/todos', todoController.getAllTodos);
+server.post('/todos',todoController.insertTodo);
+server.put('/todos',  todoController.updateTodoById);
+server.delete('/todos', todoController.deleteTodoById);
 
 server.listen(4000, function(){
     console.log('Server has started to run in express');
+    mongoose.connect(mongo_db_url)
+    .then(function(){
+        console.log('DB is connected');
+    })
+    
+    .catch(function(error){
+        console.log('DB is not connected:',error.message);
+
+    });
 });
