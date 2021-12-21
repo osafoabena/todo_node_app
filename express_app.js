3fcd98833cc48c6114d325438591c39f5d1b9b87
AbenaAOsafo <osafoabena1@gmail.com>
2021-12-21 15:29:10 +0000

    My Todo v2

--- express_app.js
+++ express_app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const todoController = require('./controllers/todoController');
+const PORT = process.env.PORT || 4000;
 const server = express();
  //const mongo_db_url ='';
 //const mongodb_db_url="";
@@ -11,12 +12,13 @@ server.use(express.json());
 
 server.listen(4000, function(){
     console.log('Server has started to run in express');
-    mongoose.connect(process.env.LOCAL_URL)
+    mongoose.connect(process.env.ATLAS_URL)
     .then(function(){
         console.log('DB is connected');
+        server.get('/', function (request, response){
+            response.status(200).json({success: true, message: 'WELCOME,this is AB todo node API'})},)
         server.get('/todos', todoController.getAllTodos);
         server.get('/todos/:id', todoController.getTodoById);
-
         server.post('/todos',todoController.insertTodo);
         server.put('/todos/:id',  todoController.updateTodoById);
         server.delete('/todos/:id', todoController.deleteTodoById);
